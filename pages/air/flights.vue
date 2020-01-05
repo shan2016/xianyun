77155b3b7de2c6deb998e8124857411ec7bbf3ab
shan <18312015269@163.com>
2020-01-05 17:01:26 +0800

    06-实现机票列表数据渲染以及分页功能
    06-实现机票列表数据渲染以及分页功能

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -15,8 +15,24 @@
                 
                 <!-- 航班信息 -->
                 <div>
-                    
+                    <FlightsItem v-for="(item,index) in dataList" :key="index" :data="item"></FlightsItem>
                 </div>
+                <el-row type="flex" justify="center" style="margin-top:10px;">
+                        <!-- size-change：切换条数时候触发 -->
+                        <!-- current-change：选择页数时候触发 -->
+                        <!-- current-page: 当前页数 -->
+                        <!-- page-size：当前条数 -->
+                        <!-- total：总条数 -->
+                        <el-pagination
+                        @size-change="handleSizeChange"
+                        @current-change="handleCurrentChange"
+                        :current-page="pageIndex"
+                        :page-sizes="[5, 10, 15, 20]"
+                        :page-size="pageSize"
+                        layout="total, sizes, prev, pager, next, jumper"
+                        :total="flightsData.total">
+                        </el-pagination>
+                    </el-row>
             </div>
 
             <!-- 侧边栏 -->
@@ -30,14 +46,50 @@
 <script>
 import moment from "moment";
 import FlightsListHead from "@/components/air/flightsListHead.vue"
+import FlightsItem from "@/components/air/flightsItem.vue"
 export default {
     data () {
         return {
-            
+            flightsData: {}, // 航班总数据
+            dataList: [] ,    // 航班列表数据，用于循环flightsItem组件，单独出来是因为要分页
+            pageIndex: 1, // 当前页数
+            pageSize: 5,  // 显示条数
         }
     },
     components: {
-        FlightsListHead
+        FlightsListHead,FlightsItem
+    },
+    methods: {
+        getData(){
+            this.$axios({
+                url:`airs`,
+                params:this.$route.query
+            }).then(res => {
+                // console.log(res)
+                this.flightsData = res.data;
+                // this.dataList = this.flightsData.flights;
+                this.setDataList();//初始化dataList数据，获取1 - 10条
+            })
+        },
+        setDataList(){
+            const start = (this.pageIndex -1)*this.pageSize;
+            const end = start +this.pageSize;
+            this.dataList=this.flightsData.flights.slice(start,end)
+        },
+        // 切换条数时触发
+        handleSizeChange(value){
+            this.pageSize = value;
+            this.pageIndex = 1;
+            this.setDataList();
+        },
+        // 切换页数时触发
+         handleCurrentChange(value){
+            this.pageIndex = value;
+            this.setDataList();
+        },
+    },
+    mounted () {
+        this.getData();
     }
 }
 </script>
